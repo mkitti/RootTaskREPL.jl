ed807e5be46b2c96156b5dccf006f3c94421497f
Mark Kittisopikul <markkitt@gmail.com>
2020-03-09 03:40:22 -0500

    redirect stdin so tests return

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,12 @@
-using RootTaskREPL
+(rd,wr) = Base.redirect_stdin()
+write(wr,"println(Base.roottask === Base.current_task());\n")
+write(wr,"@assert false\n")
+
 using Test
+using RootTaskREPL
+
+@testset "check_root_task" begin
+        @test Base.roottask === Base.current_task()
+end
+
+close(wr)
